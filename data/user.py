b611bfaaef75d7c5773f012d9e260fd935730e9d
dohun08 <bemik624@gmail.com>
2025-06-19 00:17:18 +0900

    Revert "캐시데이터 삭제"
    This reverts commit 612c1996299b4c54f5fedbf4510b6db43987ed26.

---
+++ data/user.py
@@ -0,0 +1,37 @@
+from core.supabase_client import supabase
+import random
+
+def get_students_by_partial_name(partial_name: str):
+    print(partial_name)
+    result = (
+        supabase
+        .table("student")
+        .select("name, student_number")
+        .ilike("name", f"%{partial_name}%")  # 대소문자 구분 없이 포함된 이름 검색
+        .execute()
+    )
+
+    students = result.data
+
+    combined = [{"name": student["name"], "student_number": student["student_number"]} for student in students]
+    return combined
+
+
+async def create_student(email: str, name: str, google_id: str, picture: str):
+
+    def generate_student_number():
+        return str(random.randint(100000, 999999))  # 6자리 숫자 문자열
+
+    student_number = generate_student_number()
+    existing_user = supabase.table("student").select("*").eq("email", email).execute()
+
+    if not existing_user.data:
+        supabase.table("student").insert({
+            "email": email,
+            "name": name,
+            "profile_image": picture,
+            "google_id": google_id,
+            "student_number": student_number
+        }).execute()
+
+    return True
---
+++ service/alert.py
@@ -0,0 +1,7 @@
+from data import alert as data
+
+def get_alert() :
+    return data.get_alert()
+
+def update_alert(alert_id) :
+    return data.update_alert(alert_id)
