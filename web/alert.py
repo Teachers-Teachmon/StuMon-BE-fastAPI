d8189e872d455c7c97686be5a57509d8c5d9b07d
zuu3 <ojh27467242@gmail.com>
2025-06-29 11:21:24 +0900

    feat(#27) :: 알림 도메인 완료

--- web/alert.py
+++ web/alert.py
@@ -1,11 +1,21 @@
 from fastapi import APIRouter
+from starlette.requests import Request
+from utils.auth import decode_AT
 from service import alert as service
 router = APIRouter(prefix="/alert")
 
 @router.get('')
-def get_alert() :
-    return service.get_alert()
+def get_alert(request: Request) :
+    token = request.cookies.get('access_token')
+    user_id = decode_AT(token)["user_id"]
+    return service.get_alert(user_id)
 
 @router.post('/{alert_id}')
 def update_alert(alert_id):
     return service.update_alert(alert_id)
+
+@router.delete('')
+def delete_alert(request: Request) :
+    token = request.cookies.get('access_token')
+    user_id = decode_AT(token)["user_id"]
+    return service.delete_alert(user_id)
